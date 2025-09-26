519d4bd41ba7a4e570c0150e7ff99f4285241cd6
dbcarrera <cameronliu88@gmail.com>
2025-09-26 15:15:59 +0800

    feat: implement initial structure for app features

---
+++ src/app/features/pickups/pickups.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pickups',
+  imports: [],
+  templateUrl: './pickups.html',
+  styleUrl: './pickups.css'
+})
+export class Pickups {
+
+}
