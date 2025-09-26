55ae5ca7f61e4a36e6394d91d5ca02843005dd25
dbcarrera <cameronliu88@gmail.com>
2025-09-26 16:36:30 +0800

    feat: implement routing for signup and login pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Signup } from './features/auth/pages/signup/signup';
+import { Login } from './features/auth/pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'signup', component: Signup },
+  { path: '', redirectTo: 'signup', pathMatch: 'full' },
+  { path: 'login', component: Login },
+];
