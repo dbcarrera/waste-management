bfeb887882ea1776535ba5e3bcc02d5eede041aa
MaredoB <ebamaredobalagi06@gmail.com>
2025-10-11 15:48:42 +0800

    Notifications

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Signup } from './features/auth/pages/signup/signup';
 import { Login } from './features/auth/pages/login/login';
+import { Notifications } from './features/notifications/notifications';
 
 export const routes: Routes = [
   { path: 'signup', component: Signup },
   { path: '', redirectTo: 'signup', pathMatch: 'full' },
   { path: 'login', component: Login },
+  { path: 'notifications', component: Notifications },
 ];
