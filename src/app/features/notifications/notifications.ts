aec1f11019b012e9c45530775c467bfb05f5b02b
dbcarrera <cameronliu88@gmail.com>
2025-10-13 17:59:34 +0800

    fix: get CSS working for notifications

--- src/app/features/notifications/notifications.ts
+++ src/app/features/notifications/notifications.ts
@@ -1,27 +1,18 @@
 import { Component, inject } from '@angular/core';
-import { DatePipe, NgClass } from '@angular/common';
-import { NotificationsService } from '../../core/services/notifications.service';
-import { Card } from '../../shared/components/card/card';
+import { DatePipe } from '@angular/common';
+import { NotificationsService } from './services/notifications.service';
 import { Button } from '../../shared/components/button/button';
 
 @Component({
   selector: 'app-notifications',
   standalone: true,
-  imports: [DatePipe, NgClass, Card, Button],
+  imports: [DatePipe, Button],
   templateUrl: './notifications.html',
   styleUrl: './notifications.css',
 })
 export class Notifications {
   private notificationsService = inject(NotificationsService);
   protected notifications = this.notificationsService.getNotifications();
-
-  getPriorityClass(priority: 'low' | 'medium' | 'high'): string {
-    return {
-      low: 'priority-low',
-      medium: 'priority-medium',
-      high: 'priority-high',
-    }[priority];
-  }
 
   markAsCompleted(notification: any) {
     this.notificationsService.markAsCompleted(notification);
