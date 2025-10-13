62c0c7e33386acd42a34236cfe5435b0c6a66f72
dbcarrera <cameronliu88@gmail.com>
2025-10-13 18:07:26 +0800

    feat: add login page

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FrostedCard } from '../../../../shared/components/frosted-card/frosted-card';
+import { Button } from '../../../../shared/components/button/button';
+import { FormsModule } from '@angular/forms';
+import { Header } from '../../../../shared/components/header/header';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, FrostedCard, Button, Header],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
 })
 export class Login {
+  email: string = '';
+  password: string = '';
 
+  onLogin() {
+    console.log('Login attempt with:', this.email);
+    // TOOD: Add login logic here
+  }
 }
