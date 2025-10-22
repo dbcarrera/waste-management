253cadcc5b67608ce4de0ec3c9f525689a288a8f
dbcarrera <cameronliu88@gmail.com>
2025-10-22 14:35:07 +0800

    feat: add interactivity to pickup history and improved styling

--- src/app/features/pickup-history/pickup-history.ts
+++ src/app/features/pickup-history/pickup-history.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PickupService } from '../../shared/services/pickup';
+import { Auth } from '../../shared/services/auth';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-pickups',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './pickup-history.html',
   styleUrls: ['./pickup-history.css'],
 })
 export class Pickups {
-  pickupHistory = [
-    { date: '2025-10-01', location: 'Kuala Lumpur', status: 'Completed' },
-    { date: '2025-10-03', location: 'Petaling Jaya', status: 'Pending' },
-    { date: '2025-10-05', location: 'Subang Jaya', status: 'Cancelled' },
-  ];
+  private pickupsService = inject(PickupService);
+  private authService = inject(Auth);
+
+  pickupHistory = this.authService.isAdmin()
+    ? this.pickupsService.allPickups
+    : this.pickupsService.userPickups;
+
+  completePickup(pickupId: string): void {
+    const success = this.pickupsService.completePickup(pickupId);
+    if (success) {
+      console.log('Pickup completed successfully');
+    } else {
+      console.error('Failed to complete pickup');
+    }
+  }
 }
